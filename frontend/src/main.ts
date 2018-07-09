c382be325ddf4745be2561bf0e9f41303377ce8d
wpcfan <peng.wang@outlook.com>
2018-07-09 16:07:25 +0800

    sync with so project

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,16 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { create } from 'rxjs-spy';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+const spy = create();
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
   .catch(err => console.log(err));
